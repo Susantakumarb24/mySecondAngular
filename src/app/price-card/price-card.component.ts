73a538ca70b09ddf1bef7224f0fe002f0bfe7531
Susantakumarb24 <susantakumarb24@gmail.com>
2022-04-30 21:13:51 +0530

    ok

---
+++ src/app/price-card/price-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-card',
+  templateUrl: './price-card.component.html',
+  styleUrls: ['./price-card.component.css']
+})
+export class PriceCardComponent implements OnInit {
+
+  @Input() price="";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
